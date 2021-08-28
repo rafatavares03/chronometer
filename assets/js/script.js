414cb2f4d35f431751731517ea23711beecbef8f
Rafael Tavares <rafatavares03@hotmail.com>
2021-08-28 12:24:10 -0300

    counting and pausing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 const chronometer = document.querySelector('#chronometer');
 const timer = new Date;
 
+
 function TimerZero() {
     timer.setMinutes(0);
     timer.setSeconds(0);
@@ -11,21 +12,23 @@ TimerZero();
 function WriteChronometer() {
     const minutes = (timer.getMinutes() < 10) ? '0' + timer.getMinutes() : timer.getMinutes();
     const seconds = (timer.getSeconds() < 10) ? '0' + timer.getSeconds() : timer.getSeconds();
-    const milliseconds = (timer.getMilliseconds() < 10) ? '0' + timer.getMilliseconds() : timer.getMilliseconds();
+    const milliseconds = ((timer.getMilliseconds() / 10) < 10) ? '0' + timer.getMilliseconds() / 10 : timer.getMilliseconds() / 10;
 
     chronometer.innerHTML = `${minutes}:${seconds}:${milliseconds}`;
 }
 WriteChronometer();
 
-function Counting(){
-    setInterval(() => {
+
+
+document.querySelector('#start').addEventListener('click', function Counting() {
+    timer.interval = setInterval(() => {
         let milliseconds = timer.getMilliseconds();
-        milliseconds++;
+        milliseconds += 120;
         timer.setMilliseconds(milliseconds);
-    }, 1);
-}
+        WriteChronometer();
+    }, 120);
+});
 
-
-document.querySelector('#start').addEventListener('click', Counting());
-
-setInterval(WriteChronometer, 13);
+document.querySelector('#pause').addEventListener('click', function StopCounting() {
+    clearInterval(timer.interval);
+});
